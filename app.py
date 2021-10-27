17d4c509cbafc314847cc91ef57326cf67a1daf6
vinivioti <82620421+vinivioti@users.noreply.github.com>
2021-10-27 14:09:44 -0300

    Ajsutando novo endpoint e nova msg

--- app.py
+++ app.py
@@ -7,8 +7,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Vou detonar nessa matéria !!!"
 
 if __name__ == '__main__':
    
     app.run()
+
+@app.route("/soma")
+def soma_valores():
+    x=10+10
+    return "Sua soma de 10+10="+str(x)
+
+if __name__ == '__main__':
+    app.run()    
